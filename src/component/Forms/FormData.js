22b4b827afaf046928a897985662dea59db3e9cf
khalidibwal <khalidibwal@gmail.com>
2022-04-28 21:07:50 +0700

    Summary report done

--- src/component/Forms/FormData.js
+++ src/component/Forms/FormData.js
@@ -15,55 +15,75 @@ import Swal from "sweetalert2";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 export default function FormData(props) {
   const classes = useStyles();
+  //input
+  const [myName, setMyname] = useState("");
   const [ktp, setKtp] = useState("");
+  const [emails, setEmails] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
   // Provinsi
+  const [prov, getProv] = useState("");
   const [selectProvince, getSelectedProvince] = useState("");
   const [province, setProvince] = useState([]);
   //Kota
+  const [kota, getKota] = useState("");
   const [selectCity, getSelectedCity] = useState("");
   const [city, setCity] = useState([]);
   //Kecamatan
+  const [kec, getKec] = useState("");
   const [selectkecamatan, getSelectedkecamatan] = useState("");
   const [camat, setCamat] = useState([]);
   //kelurahan
+  const [kel, getKel] = useState("");
   const [selectkelurahan, getSelectedkelurahan] = useState("");
   const [lurah, setlurah] = useState([]);
   //PostCode
-  const [postalcode, setPostalCode] = useState([])
-
-
+  const [postalcode, setPostalCode] = useState([]);
   const [gender, setGender] = useState([]);
   const [birthdate, setBirthDate] = useState(new Date());
   const MyGender = [
-    { id: 1, title: "Pria" },
-    { id: 2, title: "Wanita" },
+    { id: 1, title: "Pria", value: "Pria" },
+    { id: 2, title: "Wanita", value: "wanita" },
   ];
+  const sendname = (event) => {
+    setMyname(event.target.value);
+  };
+  const sendEmail = (event) => {
+    setEmails(event.target.value);
+  };
+  const sendAddress = (event) => {
+    setAddress(event.target.value);
+  };
+
   const handleSelect = (event) => {
     getSelectedProvince(event.target.value);
+    getProv(event.currentTarget.getAttribute("a-key"));
   };
   const citySelect = (event) => {
     getSelectedCity(event.target.value);
+    getKota(event.currentTarget.getAttribute("a-key"));
   };
-  const camatSelect = (event) =>{
-    getSelectedkecamatan(event.target.value)
-  }
-  const lurahSelect = (event) =>{
-    getSelectedkelurahan(event.target.value)
-  }
+  const camatSelect = (event) => {
+    getSelectedkecamatan(event.target.value);
+    getKec(event.currentTarget.getAttribute("a-key"));
+  };
+  const lurahSelect = (event) => {
+    getSelectedkelurahan(event.target.value);
+    getKel(event.currentTarget.getAttribute("a-key"));
+  };
   const handleKtp = (event, value) => {
     if (event.target.value.length >= 17) {
       Swal.fire({
         icon: "error",
         title: "Max Length 16 Character",
       });
+    } else {
+      setKtp(event.target.value);
     }
-    else{
-      setKtp(event.target.value)
-    }
-    console.log("ktp",ktp)
+    console.log("ktp", ktp);
   };
   const phoneMaxLength = (event, value) => {
     if (event.target.value.length >= 14) {
@@ -72,37 +92,58 @@ export default function FormData(props) {
         title: "Max Length 13 Character",
         text: "Check The Length Of Your Phone number Again",
       });
+    } else {
+      setPhone(event.target.value);
     }
   };
   const HandleProvince = () => {
     axios.get(`${APIDATA}/province`).then((res) => setProvince(res.data.data));
   };
   const navigate = useNavigate();
-  const OnSubmitData = () =>{
-    navigate('/summary',{state:{id:1,name:'sabaoon'}});
-  }
+  const OnSubmitData = () => {
+    navigate("/summary", {
+      state: {
+        id: 1,
+        name: myName,
+        ktp: ktp,
+        birth: birthdate,
+        gender: gender,
+        email: emails,
+        phone: phone,
+        address: address,
+        province: prov,
+        city: kota,
+        kecamatan: kec,
+        kelurahan: kel,
+      },
+    });
+  };
 
   useEffect(() => {
     HandleProvince();
     const HandleCity = () => {
-      axios
-        .get(`${APIDATA}/city/?province_id=${selectProvince}`)
-        .then((res) => setCity(res.data.data));
+      try {
+        axios
+          .get(`${APIDATA}/city/?province_id=${selectProvince}`)
+          .then((res) => setCity(res.data.data));
+      } catch (e) {
+        console.log("error", e);
+      }
     };
     const HandleKecamatan = () => {
       axios
         .get(`${APIDATA}/kecamatan/?city_id=${selectCity}`)
         .then((res) => setCamat(res.data.data));
     };
-    const HandleKelurahan = () =>{
+    const HandleKelurahan = () => {
       axios
         .get(`${APIDATA}/kelurahan/?kecamatan_id=${selectkecamatan}`)
         .then((res) => setlurah(res.data.data));
-    }
+    };
     HandleCity();
     HandleKecamatan();
     HandleKelurahan();
-  }, [selectProvince, selectCity,selectkecamatan]);
+  }, [selectProvince, selectCity, selectkecamatan]);
 
   return (
     <div>
@@ -114,7 +155,7 @@ export default function FormData(props) {
             name="name"
             className={classes.textField}
             helperText="Enter your full name"
-            //   onChange={handleInput}
+            onChange={sendname}
           />
           <TextField
             label="No KTP"
@@ -149,7 +190,7 @@ export default function FormData(props) {
               name="gender"
             >
               {MyGender.map((item) => (
-                <MenuItem key={item.id} value={item.id}>
+                <MenuItem key={item.id} value={item.value}>
                   {item.title}
                 </MenuItem>
               ))}
@@ -160,10 +201,9 @@ export default function FormData(props) {
             id="margin-normal"
             name="email"
             type="email"
-            //   defaultValue={formInput.name}
             className={classes.textField}
             helperText="e.g. name@gmail.com"
-            //   onChange={handleInput}
+            onChange={sendEmail}
           />
           <TextField
             label="No Telp"
@@ -178,8 +218,7 @@ export default function FormData(props) {
             label="Alamat Lengkap"
             id="margin-normal"
             name="address"
-            helperText="e.g. name@gmail.com"
-            //   onChange={handleInput}
+            onChange={sendAddress}
           />
           <FormControl fullWidth>
             <InputLabel id="demo-simple-select-label">Provinsi</InputLabel>
@@ -192,7 +231,7 @@ export default function FormData(props) {
               name="province"
             >
               {province.map((item) => (
-                <MenuItem key={item.id} value={item.id}>
+                <MenuItem key={item.id} value={item.id} a-key={item.name}>
                   {item.name}
                 </MenuItem>
               ))}
@@ -211,7 +250,7 @@ export default function FormData(props) {
                 name="city"
               >
                 {city.map((item) => (
-                  <MenuItem key={item.id} value={item.id}>
+                  <MenuItem key={item.id} value={item.id} a-key={item.name}>
                     {item.name}
                   </MenuItem>
                 ))}
@@ -233,7 +272,7 @@ export default function FormData(props) {
                 name="kecamatan"
               >
                 {camat.map((item) => (
-                  <MenuItem key={item.id} value={item.id}>
+                  <MenuItem key={item.id} value={item.id} a-key={item.name}>
                     {item.name}
                   </MenuItem>
                 ))}
@@ -254,7 +293,7 @@ export default function FormData(props) {
                 name="kelurahan"
               >
                 {lurah.map((item) => (
-                  <MenuItem key={item.id} value={item.id}>
+                  <MenuItem key={item.id} value={item.id} a-key={item.name}>
                     {item.name}
                   </MenuItem>
                 ))}
--- src/report/Summary.js
+++ src/report/Summary.js
@@ -1,5 +1,27 @@
-import React from "react";
-
-export default function Summary(){
-    
+import React,{useEffect} from "react";
+import { useLocation } from "react-router-dom";
+import { Card, CardContent, CardHeader, Avatar } from "@mui/material";
+export default function Summary() {
+  const location = useLocation();
+  return (
+    <Card sx={{ maxWidth: 500 }}>
+      <CardHeader
+        avatar={<Avatar aria-label="recipe">S</Avatar>}
+        title="Report Summary"
+        subheader={location.state.name}
+      />
+      <CardContent>Nama : {location.state.name}</CardContent>
+      <CardContent>KTP : {location.state.ktp}</CardContent>
+      <CardContent>Tanggal lahir : {location.state.birth.toDateString()}</CardContent>
+      <CardContent>Jenis Kelamin : {location.state.gender}</CardContent>
+      <CardContent>Email : {location.state.email}</CardContent>
+      <CardContent>No Telepon : {location.state.phone}</CardContent>
+      <CardContent>Alamat Lengkap : {location.state.address}</CardContent>
+      <CardContent>Provinsi : {location.state.province}</CardContent>
+      <CardContent>Kota : {location.state.city}</CardContent>
+      <CardContent>Kecamatan : {location.state.kecamatan}</CardContent>
+      <CardContent>Kelurahan : {location.state.kelurahan}</CardContent>
+      <CardContent>Postalcode : {location.state.name}</CardContent>
+    </Card>
+  );
 }
